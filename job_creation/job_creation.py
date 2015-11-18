4c76dc72cba7c4138ce24a1f39a28c2cd0ff305c
TomWerner <tomwerner>
2015-11-18 16:24:21 -0600

    fixed bugs with job creation

--- job_creation/job_creation.py
+++ job_creation/job_creation.py
@@ -6,19 +6,19 @@ import numpy as np
 def create_data_extraction_jobs(num_ensembles, image_dir, label_dir, filename_base, surrounding_pixels, python_exe):
     # First find the number of images in this directory
     file_list = [f for f in listdir(image_dir) if isfile(join(image_dir, f))]
-    min_number = min(file_list, key=lambda x: int(str(x)[2: -4]))
-    max_number = max(file_list, key=lambda x: int(str(x)[2: -4]))
-
+    min_number = int(min(file_list, key=lambda x: int(str(x)[2: -4]))[2: -4])
+    max_number = int(max(file_list, key=lambda x: int(str(x)[2: -4]))[2: -4]) + 1
+    
     image_groups = np.split(np.arange(min_number, max_number), num_ensembles)
     data_ext_script_loc = "~/skin_detection/data_extraction/data_extraction.py"
 
     for image_group in image_groups:
         data_ext_call = " ".join([python_exe, data_ext_script_loc,
-                                  min(image_group), max(image_group),
+                                  str(min(image_group)), str(max(image_group)),
                                   image_dir,
                                   '-l', label_dir,
-                                  '-o', "~/extracted_data/" + filename_base + "_" + min(image_group) + "_" + max(image_group),
-                                  '-p', surrounding_pixels])
+                                  '-o', "~/extracted_data/" + filename_base + "_" + str(min(image_group)) + "_" + str(max(image_group)),
+                                  '-p', str(surrounding_pixels)])
 #         print("""
 # #!/bin/sh
 #
@@ -48,7 +48,7 @@ if __name__ == "__main__":
               "--num_pixels ##\n " +
               "--python_exe xxx\n " +
               "--filename_base xxx\n ")
-    num_ensembles = int(parse_arg("--num_ensembes", sys.argv, 1))
+    num_ensembles = int(parse_arg("--num_ensembles", sys.argv, 1))
     image_dir = parse_arg("--image_dir", sys.argv, "/Shared/bdagroup3/Original/train/")
     label_dir = parse_arg("--label_dir", sys.argv, "/Shared/bdagroup3/Skin/train/")
     validation_image_dir = parse_arg("--v_image_dir", sys.argv, "/Shared/bdagroup3/Original/val/")
