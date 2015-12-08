238e06dc09b3eacef1a8dc4930de5fd4a7a6352b
TomWerner <tomwerner>
2015-12-08 14:58:56 -0600

    going back

--- op_elm/OP_ELM.py
+++ op_elm/OP_ELM.py
@@ -139,10 +139,10 @@ class ELM(SLFN):
         super(ELM, self).__init__(data.shape[1], targets.shape[1])
         if not inputs_normalized:
             start = time.time()
-            data = np.asarray(data, dtype=float)  # preprocessing.scale(np.asarray(data, dtype=float), axis=0)
-            targets = np.asarray(data, dtype=float)  # preprocessing.scale(np.asarray(targets, dtype=float), axis=0)
-            data /= 255
-            targets /= 255
+            data = preprocessing.scale(np.asarray(data, dtype=float), axis=0)
+            targets = preprocessing.scale(np.asarray(targets, dtype=float), axis=0)
+            # data /= 255
+            # targets /= 255
             print("Finished normalizing data:", (time.time() - start))
 
         self.data = data
